0257953691369ea0f079eee76a4a9cebf992530b
Gerard <gerard.kruisheer@gmail.com>
2024-09-23 19:58:23 +0000

    Added heating algo

--- floor-heating-planner/src/components/DraggableRoom.js
+++ floor-heating-planner/src/components/DraggableRoom.js
@@ -1,16 +1,24 @@
 // src/components/DraggableRoom.js
 
 import React from 'react';
+import RoomCanvas from './RoomCanvas';
+import { createRoomGrid } from '../utils/roomGrid';
+import { generateHeatingLoopPath } from '../utils/pathGenerator';
 import './DraggableRoom.css'; // Ensure this import statement is correct
 
-const DraggableRoom = ({ name, dimensions }) => {
+const DraggableRoom = ({ name, dimensions, obstacles = [] }) => {
+  const grid = createRoomGrid(dimensions);
+  const gridWithObstacles = addObstaclesToGrid(grid, obstacles);
+  const path = generateHeatingLoopPath(gridWithObstacles);
+
   return (
     <div className="draggable-room">
       <h4>{name}</h4>
       <p>{dimensions}</p>
-      {/* Additional features like drag handles can be added here */}
+      <RoomCanvas grid={gridWithObstacles} path={path} />
     </div>
   );
 };
+
 
 export default DraggableRoom;
